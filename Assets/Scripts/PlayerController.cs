909c42ec30a329ef5370826d3b77fbac26e47d3e
Potato <allanmanuba@gmail.com>
2018-12-09 01:27:59 -0700

    Change player input to keys
    Issue: Player slows down cornering

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float movementFrame = 10.0f;
     public float incrementRadius = 0.5f;
     public GameObject gamePlane;
+    public NavMeshAgent agent;
 
     private readonly int IND_HORIZONTAL = 1;
     private readonly int IND_VERTICAL = 0;
@@ -23,10 +25,10 @@ public class PlayerController : MonoBehaviour
     private readonly int IND_LEFT = 2;
     private readonly int IND_RIGHT = 3;
 
-    private readonly int BIT_UP = 1;
-    private readonly int BIT_DOWN = 2;
-    private readonly int BIT_LEFT = 4;
-    private readonly int BIT_RIGHT = 8;
+    private static readonly int BIT_UP = 1;
+    private static readonly int BIT_DOWN = 2;
+    private static readonly int BIT_LEFT = 4;
+    private static readonly int BIT_RIGHT = 8;
 
     private int[] BIT_DIR_ARRAY; // WARNING: BIT_LEFT AND RIGHT ARE REVERSED HERE
     private Vector3 staticCenter;
@@ -34,20 +36,18 @@ public class PlayerController : MonoBehaviour
     private int validDirection; // use bitwise
     private Vector3 destination;
 
-    private float increment;
+    public float mainIncrement;
+    public float secondaryIncrement;
 
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
-        increment = incrementRadius / movementFrame;
         BIT_DIR_ARRAY = new int[] { BIT_UP, BIT_DOWN, BIT_RIGHT, BIT_LEFT };
         currentDirection = BIT_RIGHT;
         validDirection = BIT_RIGHT | BIT_LEFT;
         staticCenter = CloneVector(transform.position);
-
-        SetDestination(currentDirection);
     }
 
     private Vector3 CloneVector(Vector3 vector)
@@ -60,29 +60,93 @@ public class PlayerController : MonoBehaviour
         return (val1 & val2) > 0;
     }
 
-    private void SetDestination(int directionBitwise)
+    private void SetIndTarget(int direction, float increment, float x, float z,
+        out float x2, out float z2)
     {
-        if (AndBitwise(directionBitwise, BIT_UP))
+        if (AndBitwise(direction, BIT_UP))
         {
-            destination = new Vector3(staticCenter.x, staticCenter.y, staticCenter.z + incrementRadius);
+            z += increment;
         }
-        else if (AndBitwise(directionBitwise, BIT_UP))
+        else if (AndBitwise(direction, BIT_DOWN))
         {
-            destination = new Vector3(staticCenter.x, staticCenter.y, staticCenter.z - incrementRadius);
+            z -= increment;
         }
-        else if (AndBitwise(directionBitwise, BIT_LEFT))
+
+        if (AndBitwise(direction, BIT_RIGHT))
         {
-            destination = new Vector3(staticCenter.x - incrementRadius, staticCenter.y, staticCenter.z);
+            x += increment;
         }
-        else if (AndBitwise(directionBitwise, BIT_RIGHT))
+        else if (AndBitwise(direction, BIT_LEFT))
         {
-            destination = new Vector3(staticCenter.x + incrementRadius, staticCenter.y, staticCenter.z);
+            x -= increment;
+        }
+
+        x2 = x;
+        z2 = z;
+    }
+
+    private Vector3 SetTarget(int mainDirection, int secondaryDirection)
+    {
+        Vector3 currentPosition = transform.position;
+        float x = currentPosition.x;
+        float y = currentPosition.y;
+        float z = currentPosition.z;
+
+        SetIndTarget(mainDirection, mainIncrement, x, z, out x, out z);
+        SetIndTarget(secondaryDirection, secondaryIncrement, x, z, out x, out z);
+
+        return new Vector3(x,y,z);
+    }
+
+    private int[] horizontalOptions = new int[] { 0, BIT_LEFT, BIT_RIGHT };
+    private int[] verticalOptions = new int[] { 0, BIT_UP, BIT_DOWN };
+    public float sampleMovementRadius;
+
+    // if returns false, is vertical
+    // assumes only one direction
+    private bool IsHorizontal(int direction)
+    {
+        return AndBitwise(direction, BIT_LEFT | BIT_RIGHT);
+    }
+
+    private void SetDestination(int mainBit)
+    {
+        int[] optionalBits = horizontalOptions;
+        if (IsHorizontal(mainBit))
+        {
+            optionalBits = verticalOptions;
+        } // else option already done
+
+        foreach (int secondaryBit in optionalBits)
+        {
+            Vector3 tmpDestination = SetTarget(mainBit, secondaryBit);
+            NavMeshHit hit;
+            //print(tmpDestination.ToString());
+
+            bool isHit = NavMesh.SamplePosition(tmpDestination, out hit, sampleMovementRadius, NavMesh.AllAreas);
+            if (isHit)
+            {
+                //print("Detects hit");
+                agent.SetDestination(hit.position);
+                break;
+            }
         }
     }
 
-    private bool IsDirectionHorizontal()
+    private void CheckDestination(int directionBitwise)
     {
-        return AndBitwise(currentDirection, BIT_RIGHT | BIT_LEFT);
+        int mainBit = 0;
+        
+        foreach (int bitDirection in BIT_DIR_ARRAY)
+        {
+            mainBit = directionBitwise & bitDirection;
+
+            if (mainBit != 0)
+            {
+                SetDestination(mainBit);
+                break;
+            }
+        }
     }
 
     private void Update()
@@ -99,42 +163,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        // change movement based on control
-        bool isValidDirection = AndBitwise(validDirection, movementInput);
-        bool isDifferentInput = !AndBitwise(currentDirection, movementInput);
-        if (isValidDirection && isDifferentInput)
-        {
-            // set new destination
-            SetDestination(movementInput);
-        }
-
-
-        // go to center (how and when)
-        // if aligned to destination
-        Vector3 currentPosition = transform.position;
-        if (Mathf.Approximately(destination.x, currentPosition.x) ||
-            Mathf.Approximately(destination.z, currentPosition.z))
-        {
-            // move to desination
-           if (currentPosition.z > destination.z)
-            {
-
-            }
-        }
-        else
-        {
-            // move to center
-
-        }
-
-        // go to destination
-
-        // set position
-
-
-        // check if reach destination
-
-        // if true, change static center
+        CheckDestination(movementInput);
     }
 
     void FixedUpdate()
