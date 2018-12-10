7055397521e2da91d5a205793cf600595c18b3e9
Potato <allanmanuba@gmail.com>
2018-12-10 00:02:57 -0700

    Revert "Change player input to keys"
    This reverts commit 909c42ec30a329ef5370826d3b77fbac26e47d3e.
    
    # Conflicts:
    #	Assets/Scenes/Scene2.unity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,13 @@ using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float movementFrame = 10.0f;
     public float incrementRadius = 0.5f;
-    public GameObject gamePlane;
     public NavMeshAgent agent;
 
     private readonly int IND_HORIZONTAL = 1;
     private readonly int IND_VERTICAL = 0;
     private readonly string[] AXIS_ARGS = { "Vertical", "Horizontal" };
-
-    private Rigidbody rb;
-    private float[] directions = { 1, 0 };
-    private float spawnOffset = 0;
-    private float SPAWN_POSITION_LIMIT = 0;
-
     private readonly int[] COORD_RANGE = new int[]{0, 1};
 
     private readonly int IND_UP = 0;
@@ -25,29 +17,32 @@ public class PlayerController : MonoBehaviour
     private readonly int IND_LEFT = 2;
     private readonly int IND_RIGHT = 3;
 
-    private static readonly int BIT_UP = 1;
-    private static readonly int BIT_DOWN = 2;
-    private static readonly int BIT_LEFT = 4;
-    private static readonly int BIT_RIGHT = 8;
+    private readonly static int BIT_UP = 1;
+    private readonly static int BIT_DOWN = 2;
+    private readonly static int BIT_LEFT = 4;
+    private readonly static int BIT_RIGHT = 8;
 
-    private int[] BIT_DIR_ARRAY; // WARNING: BIT_LEFT AND RIGHT ARE REVERSED HERE
-    private Vector3 staticCenter;
-    private int currentDirection;
+    private readonly static int[] BIT_DIR_ARRAY = 
+        new int[] { BIT_UP, BIT_DOWN, BIT_RIGHT, BIT_LEFT };
+    // WARNING: BIT_LEFT AND RIGHT ARE REVERSED HERE
     private int validDirection; // use bitwise
     private Vector3 destination;
-
+ 
+    private float increment;
     public float mainIncrement;
     public float secondaryIncrement;
+    private Vector3 staticCenter;
+    private int currentDirection;
 
     // Use this for initialization
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-
-        BIT_DIR_ARRAY = new int[] { BIT_UP, BIT_DOWN, BIT_RIGHT, BIT_LEFT };
+        increment = incrementRadius / movementFrame;
         currentDirection = BIT_RIGHT;
         validDirection = BIT_RIGHT | BIT_LEFT;
         staticCenter = CloneVector(transform.position);
+
+        SetDestination(currentDirection);
     }
 
     private Vector3 CloneVector(Vector3 vector)
@@ -60,29 +55,51 @@ public class PlayerController : MonoBehaviour
         return (val1 & val2) > 0;
     }
 
-    private void SetIndTarget(int direction, float increment, float x, float z,
+    private void SetIndTarget(int direction, float increment, float x, float z, 
         out float x2, out float z2)
     {
         if (AndBitwise(direction, BIT_UP))
         {
+            destination = new Vector3(staticCenter.x, staticCenter.y, staticCenter.z + incrementRadius);
             z += increment;
         }
         else if (AndBitwise(direction, BIT_DOWN))
         {
+            destination = new Vector3(staticCenter.x, staticCenter.y, staticCenter.z - incrementRadius);
             z -= increment;
         }
-
         if (AndBitwise(direction, BIT_RIGHT))
         {
+            destination = new Vector3(staticCenter.x - incrementRadius, staticCenter.y, staticCenter.z);
             x += increment;
         }
         else if (AndBitwise(direction, BIT_LEFT))
         {
             x -= increment;
         }
-
+        
         x2 = x;
         z2 = z;
+    }
+
+    private void SetNewDestination(int directionBitwise)
+    {
+        if (AndBitwise(directionBitwise, BIT_UP))
+        {
+            destination = new Vector3(staticCenter.x, staticCenter.y, staticCenter.z + mainIncrement);
+        }
+        else if (AndBitwise(directionBitwise, BIT_UP))
+        {
+            destination = new Vector3(staticCenter.x, staticCenter.y, staticCenter.z - mainIncrement);
+        }
+        else if (AndBitwise(directionBitwise, BIT_LEFT))
+        {
+            destination = new Vector3(staticCenter.x - mainIncrement, staticCenter.y, staticCenter.z);
+        }
+        else if (AndBitwise(directionBitwise, BIT_RIGHT))
+        {
+            destination = new Vector3(staticCenter.x + mainIncrement, staticCenter.y, staticCenter.z);
+        }
     }
 
     private Vector3 SetTarget(int mainDirection, int secondaryDirection)
@@ -91,11 +108,18 @@ public class PlayerController : MonoBehaviour
         float x = currentPosition.x;
         float y = currentPosition.y;
         float z = currentPosition.z;
-
+        
         SetIndTarget(mainDirection, mainIncrement, x, z, out x, out z);
         SetIndTarget(secondaryDirection, secondaryIncrement, x, z, out x, out z);
+        
+        return new Vector3(x, y, z);
+    }
 
-        return new Vector3(x,y,z);
+
+
+    private bool IsDirectionHorizontal()
+    {
+        return AndBitwise(currentDirection, BIT_RIGHT | BIT_LEFT);
     }
 
     private int[] horizontalOptions = new int[] { 0, BIT_LEFT, BIT_RIGHT };
@@ -113,7 +137,7 @@ public class PlayerController : MonoBehaviour
     {
         int[] optionalBits = horizontalOptions;
         if (IsHorizontal(mainBit))
-        {
+         {
             optionalBits = verticalOptions;
         } // else option already done
 
@@ -125,30 +149,30 @@ public class PlayerController : MonoBehaviour
 
             bool isHit = NavMesh.SamplePosition(tmpDestination, out hit, sampleMovementRadius, NavMesh.AllAreas);
             if (isHit)
-            {
+             {
                 //print("Detects hit");
                 agent.SetDestination(hit.position);
                 break;
-            }
-        }
+             }
+         }
     }
-
+ 
     private void CheckDestination(int directionBitwise)
     {
         int mainBit = 0;
         
         foreach (int bitDirection in BIT_DIR_ARRAY)
-        {
+         {
             mainBit = directionBitwise & bitDirection;
-
+ 
             if (mainBit != 0)
-            {
+             {
                 SetDestination(mainBit);
                 break;
-            }
-        }
+             }
+         }
     }
-
+ 
     private void Update()
     {
         // control
@@ -162,12 +186,8 @@ public class PlayerController : MonoBehaviour
                 movementInput = movementInput | BIT_DIR_ARRAY[(index * 2) + factor];
             }
         }
-
+ 
         CheckDestination(movementInput);
-    }
+     }
 
-    void FixedUpdate()
-    {   
-
-    }
 }
